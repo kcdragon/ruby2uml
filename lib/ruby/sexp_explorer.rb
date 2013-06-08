3231fc8b751e1d9b22b8ed15666888d05bdc4592
Mike Dalton <michaelcdalton@gmail.com>
2013-06-08 10:41:56 -0400

    added support for ruby programs that call other classes and one to one instance variables, added test cases

--- lib/ruby/sexp_explorer.rb
+++ lib/ruby/sexp_explorer.rb
@@ -5,9 +5,13 @@ module Ruby
   # NOTE: "head" and "rest" (aliased from "sexp_type" and "sexp_body") method calls on Sexp objects are simliar to Lisp's functions "car" and "cdr"
   # contains information to tell where in the sexp everything is for ruby
   class SexpExplorer
-
     def initialize
-      @parent_relationship = ParentRelationship.new
+      # REFACTOR VIOLATES open-close principle, need to have relationships register with SexpExplorer
+      @ef = Graph::EdgeFactory.instance
+      @rels = Array.new
+      @rels << ParentRelationship.new
+      @rels << DependencyRelationship.new
+      @rels << AggregationRelationship.new
     end
 
     def get_subject
@@ -15,13 +19,17 @@ module Ruby
     end
     
     def each sexp, &block
-      @parent_relationship.each sexp, &block
+      @rels.each do |rel|
+        rel.each sexp, &block
+      end
     end
   end
 
   # REFACTOR extract class into file(s)
   class Relationship
-    
+    def initialize
+      @ef = Graph::EdgeFactory.instance
+    end
   end
 
   class ParentRelationship < Relationship
@@ -31,30 +39,73 @@ module Ruby
       if parent_node != nil # class has a parent
         parent = parent_node.rest.head
       end
-      
-      # REFACTOR out edge factory into instance variable
+ 
+      # REFACTOR out edge factory into instance variable     
+      yield_wrapper = lambda { return parent, Graph::ClassVertex, @ef.get_edge(:generalization) }
       if block_given?
-        block.call(parent, Graph::ClassVertex, Graph::EdgeFactory.instance.get_edge(:generalization))
+        block.call yield_wrapper.call
       else
-        yield parent, Graph::ClassVertex, Graph::EdgeFactory.instance.get_edge(:generalization)
+        yield yield_wrapper.call
       end
     end
   end
 
-  # TODO implement aggregation
-  class AggregationRelationship < Relationship
+  # TODO implement implements, will take implements to mean "class Foo include Bar ; end"
+  class ImplementsRelationship
+    def each sexp, &block
+    end
   end
 
-  # TODO implement dependency
-  #  class_node.each_of_type(:call) do |call_node|
-  #    call_node.sexp_body.each_of_type(:const) do |dependency_node|
-  #      dependency_name = dependency_node.rest.head.to_s
-  #      depends_on << dependency_name
-  #    end
-  #  end
-  # depends_on.each do |d|
-  #   klass.add_edge @edge_factory.get_edge(:dependency), d
-  # end
+  # TODO implement aggregation
+  # TODO implement for instance variabels, i.e. @ vars
+  # TODO implement for class variables, i.e. @@ vars
+  class AggregationRelationship < Relationship
+    def each sexp, &block
+      sexp.each_of_type(:defn) do |method_node|
+        
+        get_method_body(method_node).each_sexp do |sub_sexp|
+          case sub_sexp.first
+          when :iasgn
+            rhs = sub_sexp.rest.rest #right-hand-side of assignment
+            rhs.each_of_type(:const) do |node|
+              name = node.rest.head
+
+              # REFACTOR extract method to superclass
+              yield_wrapper = lambda { return name, Graph::ClassVertex, @ef.get_edge(:aggregation) }
+              if block_given?
+                block.call yield_wrapper.call
+              else
+                yield yield_wrapper.call
+              end
+
+            end
+          end
+        end
+
+      end
+    end
+    
+  private
+    # precondition: sexp is a method node
+    def get_method_body sexp
+      # three "rest" calls will skip over type, method name and arguments
+      sexp.rest.rest.rest
+    end
+  end
+
   class DependencyRelationship < Relationship
+    def each sexp, &block
+      sexp.each_of_type(:call) do |call_node|
+        call_node.rest.each_of_type(:const) do |dependency_node|
+          dependency_name = dependency_node.rest.head
+           yield_wrapper = lambda { return dependency_name, Graph::ClassVertex, @ef.get_edge(:dependency) }
+          if block_given?
+            block.call yield_wrapper.call
+          else
+            yield yield_wrapper.call
+          end
+        end
+      end
+    end
   end
 end
--- test/test_ruby_sexp.rb
+++ test/test_ruby_sexp.rb
@@ -12,18 +12,82 @@ class TestRubySexp < Test::Unit::TestCase
     @edge_factory = Graph::EdgeFactory.instance
   end
 
-  def test_ruby_program_with_inheritance
-    program = "class Foo < Bar ; end"
+private
+  # return GraphGenerator
+  def analyze_program program
     sexp = SexpFactory.instance.get_sexp program, 'rb'
     graph = Graph::Digraph.new
     generator = GraphGenerator.new graph, @explorer, @edge_factory
     generator.analyze_sexp sexp
-    
-    assert graph.has_vertex? :Foo
-    assert graph.has_vertex? :Bar
+    return generator
+  end
+
+public
+  # NOTE these are not unit tests, they are integration tests, i currently don't have many unit tests for the individual modules
+  # TODO move these into an integration folder
+
+  def test_program_with_inheritance
+    program = "class Foo < Bar ; end"
+    graph = analyze_program(program).graph
+
+    foo, bar = assert_and_get_vertices graph, :Foo, :Bar
+    assert_edge_type foo, bar, :generalization
+  end
+
+  def test_program_with_dependency
+    program = "class Foo \n def hello \n return Bar.new \n end \n end"
+    graph = analyze_program(program).graph
+
+    foo, bar = assert_and_get_vertices graph, :Foo, :Bar
+    assert_edge_type foo, bar, :dependency
+  end
+
+  def test_program_with_instance_variable_aggregation_one_to_one
+    program = <<-EOS
+      class Foo
+        def initialize
+          @bar = Bar.new
+        end
+      end
+    EOS
+    graph = analyze_program(program).graph
+
+    foo, bar = assert_and_get_vertices graph, :Foo, :Bar
+    assert_edge_type foo, bar, :aggregation
+  end
+
+#  def test_program_with_instance_variable_aggregation_one_to_many
+#    program = <<-EOS
+#      class Foo
+#        def initialize
+#          @bar = Array.new
+#          @bar << Bar.new
+#          @bar << Bar.new
+#        end
+#      end
+#    EOS
+#    graph = analyze_program(program).graph
+
+#    foo, bar, array = assert_and_get_vertices graph, :Foo, :Bar, :Array
+#    assert_edge_type foo, array, :aggregation
+#    assert_edge_type foo, bar, :aggregation
+#  end
+
+private
+  # precondition: vertices is non-empty
+  # postcondition: returns n vertex objects s.t. n is vertices.length
+  def assert_and_get_vertices graph, *vertices
+    vertices.each do |v|
+      assert graph.has_vertex?(v), "graph must contain vertex #{v}"
+    end
     
     foo = graph.get_vertex :Foo
     bar = graph.get_vertex :Bar
-    assert foo.get_edge(@edge_factory.get_edge(:generalization)).include?(bar)
+    return foo, bar
+  end
+
+  def assert_edge_type from, to, type
+    e = @edge_factory.get_edge(type)
+    assert from.get_edge(e).include?(to), "#{from.name} #{e.to_s} #{to.name} must be true"
   end
 end
