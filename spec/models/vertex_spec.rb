5bf3640936d566c19256c102e18ab7b87c3be0e3
Mike Dalton <michaelcdalton@gmail.com>
2013-06-13 21:45:34 -0400

    changed enumeration test to a spec, removed test_vertex since it is covered by vertex_spec

--- spec/models/vertex_spec.rb
+++ spec/models/vertex_spec.rb
@@ -2,8 +2,9 @@ require_relative '../../lib/graph/edge_factory'
 require_relative '../../lib/graph/vertex'
 
 describe Graph::Vertex do
-  before :all do
+  before :each do
     @vertex = Graph::Vertex.new 'foo'
+    @ef = Graph::EdgeFactory.instance
   end
   subject { @vertex }
 
@@ -13,11 +14,32 @@ describe Graph::Vertex do
   it { should respond_to :[] }
 
   it "should add aggregation edge" do
-    ef = Graph::EdgeFactory.instance
     another_vertex = Graph::Vertex.new 'bar'
-    edge = ef.get_edge :aggregation
+    edge = @ef.get_edge :aggregation
     lambda {
       subject.add_edge edge, another_vertex
-    }.should change(subject[ef.get_edge :aggregation], :count).by(1)
+    }.should change(subject[@ef.get_edge :aggregation], :count).by(1)
+  end
+
+  it "should enumerate edges" do
+    @one = Graph::ClassVertex.new 'one'
+    @two = Graph::ClassVertex.new 'two'
+    @three = Graph::ClassVertex.new 'three'
+    @vertex.add_edge @ef.get_edge(:generalization), @one
+    @vertex.add_edge @ef.get_edge(:aggregation), @two
+    @vertex.add_edge @ef.get_edge(:aggregation), @three
+    
+    actual_gen = Array.new
+    actual_agg = Array.new
+    @vertex.each do |edge, set|
+      if edge.eql? @ef.get_edge(:generalization)
+        actual_gen.concat set.to_a
+      elsif edge.eql? @ef.get_edge(:aggregation)
+        actual_agg.concat set.to_a
+      end
+    end
+    
+    expect(actual_gen).to match_array([@one])
+    expect(actual_agg).to match_array([@two, @three])
   end
 end
