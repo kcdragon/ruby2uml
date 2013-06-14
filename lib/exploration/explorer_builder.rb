3ce872cf83946f7eff5baec9f7aba5639164ab8e
Mike Dalton <michaelcdalton@gmail.com>
2013-06-13 22:49:00 -0400

    documentation and comments

--- lib/exploration/explorer_builder.rb
+++ lib/exploration/explorer_builder.rb
@@ -7,13 +7,21 @@ require_relative 'dependency_relation'
 require_relative 'parent_relation'
 
 module Exploration
-  # All build methods must return an object that implements Explorable
+
+  # All build methods must return an object that implements Explorable.
   class ExplorerBuilder
     def self.instance
       @@instance ||= ExplorerBuilder.new
     end
     
-    # Build explorer for Ruby programs
+    # Builds Explorer for Ruby programs. This explorer picks up:
+    #
+    # * Classes
+    #   * Generalization relationships
+    #   * Aggregation relationships
+    #   * Dependency relationships
+    # * Modules
+    #   * Dependency relationships
     def build_ruby_explorer
       agg = AggregationRelation.new
       dep = DependencyRelation.new
@@ -23,7 +31,6 @@ module Exploration
       class_entity.add_explorer agg
       class_entity.add_explorer dep
       class_entity.add_explorer par
-
       module_entity = ModuleEntity.new
       module_entity.add_explorer class_entity
       module_entity.add_explorer dep
