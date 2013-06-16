1b00989c93f74a98a73c4688d5233843a2f7548f
Mike Dalton <michaelcdalton@gmail.com>
2013-06-16 12:35:46 -0400

    added each_child extension to sexp, added spec for each_child

---
+++ lib/sexp_ext.rb
@@ -0,0 +1,20 @@
+class Sexp
+  def each_child &block
+    # REFACTOR i don't like this switch statement
+    type = self.head
+    if type == :class
+      children = rest.rest.rest
+    elsif type == :module
+      children = rest.rest
+    else
+      raise "Unsupported type for each_child method: #{type}"
+    end
+
+    child = children.first
+    while child != nil
+      block.call child
+      children = children.rest
+      child = children.first
+    end
+  end
+end
