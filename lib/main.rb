bfcdf338f03fe046f71e1e0eb9f70f252c89b65d
Mike Dalton <michaelcdalton@gmail.com>
2013-06-23 16:05:23 -0400

    changed file name to a command line argument rather than hard coding it in main.rb

--- lib/main.rb
+++ lib/main.rb
@@ -1,22 +1,19 @@
-require 'pp'
 require 'yaml'
 
-require_relative 'graph/digraph'
-require_relative 'exploration/class_entity'
 require_relative 'exploration/explorer_builder'
-require_relative 'exploration/aggregation_relation'
-require_relative 'exploration/dependency_relation'
-#require_relative 'exploration/implements_relation'
-require_relative 'exploration/parent_relation'
 require_relative 'graph_generator'
 require_relative 'sexp_factory'
 require_relative 'uml/dot_builder'
 
 config = YAML.load_file 'config/dot.yml'
 
-# REFACTOR into command line arguments
-FILE_NAME = 'data/edge.rb'
-file = File.open FILE_NAME, 'rb' # open file as binary to read into one string
+if ARGV.length == 0
+  puts 'Must include a file name as an argument.'
+  exit
+end
+
+file_name = ARGV[0]
+file = File.open file_name, 'rb' # open file as binary to read into one string
 program = file.read
 
 sexp = SexpFactory.instance.get_sexp program, 'rb'
