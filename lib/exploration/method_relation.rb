c2b14bdf15ae1ec0202ce289b250da924b08fc9c
Mike Dalton <michaelcdalton@gmail.com>
2013-06-29 11:38:59 -0400

    pulled down each_of_type method since it is only used in MethodRelation

--- lib/exploration/method_relation.rb
+++ lib/exploration/method_relation.rb
@@ -15,5 +15,25 @@ module Exploration
         each_of_type type, sexp, context, explorers, &block
       end
     end
+
+    # Examines each child of +sexp+ and explores that child if it matches +type+. Searches the child for matches of the keys in +types_to_search+ and calls the Proc on that sexp for any matches. The Proc knows how to explore the given type.
+    #
+    # [params] - type is a sexp type (ex. :module, :class)
+    #          - sexp is a Sexp object
+    #          - context is a Hash with attributes describing the parent Sexp
+    #          - types_to_search is a Hash of sexp types mapped to Procs
+    def each_of_type type, sexp, context, types_to_search, &block
+      sexp.each_child do |sub_sexp|
+        if sub_sexp.first == type
+          method_node = sub_sexp
+          method_body = method_node.rest.rest.rest
+          method_body.deep_each do |sub_sexp|
+            types_to_search.each do |type, explorer|
+              explorer.call(sub_sexp) if type.include? sub_sexp.first
+            end
+          end
+        end
+      end
+    end
   end
 end
--- lib/exploration/relation.rb
+++ lib/exploration/relation.rb
@@ -4,26 +4,6 @@ require_relative 'explorer'
 module Exploration
   class Relation < Explorer
 
-    # Examines each child of +sexp+ and explores that child if it matches +type+. Searches the child for matches of the keys in +types_to_search+ and calls the Proc on that sexp for any matches. The Proc knows how to explore the given type.
-    #
-    # [params] - type is a sexp type (ex. :module, :class)
-    #          - sexp is a Sexp object
-    #          - context is a Hash with attributes describing the parent Sexp
-    #          - types_to_search is a Hash of sexp types mapped to Procs
-    def each_of_type type, sexp, context, types_to_search, &block
-      sexp.each_child do |sub_sexp|
-        if sub_sexp.first == type
-          method_node = sub_sexp
-          method_body = method_node.rest.rest.rest
-          method_body.deep_each do |sub_sexp|
-            types_to_search.each do |type, explorer|
-              explorer.call(sub_sexp) if type.include? sub_sexp.first
-            end
-          end
-        end
-      end
-    end
-
     def explore_sexp_with_namespace sexp, type, context, already_explored, &block
       sexp.each_of_type(:colon2) do |namespace_sexp|
         if !already_explored.include?(namespace_sexp)
