73c750d935d3ce2092301193393921af5ec280b9
Mike Dalton <michaelcdalton@gmail.com>
2013-06-29 17:14:23 -0400

    added spec for two classes wrapped in block, renamed file to be more consistent with other integration spec names

---
+++ spec/integration/block_wrapper_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../../lib/graph/vertex'
+require_relative 'graph_generator_helper'
+
+# When a program has multiple elements on the top level (i.e. two classes not nested in a module, require statements followed by a class), the whole program is wrapped in a block when a Sexp is generated. These specs test programs that have a block wrapping the whole program.
+describe "Block Wrappers" do
+  include GraphGeneratorHelper
+
+  let(:foo) { Graph::Vertex.new 'Foo', :class }
+  let(:bar) { Graph::Vertex.new 'Bar', :class }
+
+  it "generates graph that begins with require statements" do
+    program = "require 'set'; class Foo; end"
+    graph = generate_graph program
+    expect(graph.each.to_a).to match_array [foo]
+  end
+
+  it "generates a graph with multiple classes" do
+    program = "class Foo; end; class Bar; end"
+    graph = generate_graph program
+    expect(graph.each.to_a).to match_array [foo, bar]
+  end
+end
