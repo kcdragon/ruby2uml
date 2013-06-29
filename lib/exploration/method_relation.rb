f2731f82fcdcbf7624e30c6145a51ed826b5a036
Mike Dalton <michaelcdalton@gmail.com>
2013-06-29 13:19:42 -0400

    removing constants

--- lib/exploration/method_relation.rb
+++ lib/exploration/method_relation.rb
@@ -7,12 +7,9 @@ module Exploration
   # get_method_explorers returns a map with keys equal to the sexp types to explorer and the corresponding values as Procs that know how to explore that sexp.
   class MethodRelation < Relation
 
-    INSTANCE_METHOD_TYPE = :defn
-    CLASS_METHOD_TYPE = :defs
-
     def each sexp, context=nil, &block
       explorers = get_method_explorers context, &block
-      [INSTANCE_METHOD_TYPE, CLASS_METHOD_TYPE].each do |type|
+      [:defn, :defs].each do |type|
         each_of_type type, sexp, context, explorers, &block
       end
     end
