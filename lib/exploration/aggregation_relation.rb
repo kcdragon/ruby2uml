c609dded8b6e8a4bec3158bf88dcfd2d1d45c1f2
Mike Dalton <michaelcdalton@gmail.com>
2013-06-30 18:18:54 -0400

    removed old method

--- lib/exploration/aggregation_relation.rb
+++ lib/exploration/aggregation_relation.rb
@@ -1,7 +1,6 @@
 require_relative 'relation'
 
 module Exploration
-  # TODO implement 1-n aggregation, punting on it for now
   class AggregationRelation < Relation
 
     def each sexp, context=nil, &block
@@ -15,20 +14,6 @@ module Exploration
           explore_entity_sexp :const, sexp, :aggregation, context, already_explored, &block
         end
       end
-    end
-
-    # Yields aggregation relationships in +sexp+
-    def get_method_explorers context, &block
-      already_explored = []
-      {
-        [:iasgn, :cvasgn] => lambda do |sexp| # search inside instance and class assignment variables
-          rhs = sexp.rest.rest # right-hand-side of assignment
-
-          # by exploring :colon2 first, we won't pick up any :const that was inside a :colon2
-          explore_entity_sexp :colon2, sexp, :aggregation, context, already_explored, &block
-          explore_entity_sexp :const, sexp, :aggregation, context, already_explored, &block
-        end
-      }
     end
   end
 end
--- lib/exploration/dependency_relation.rb
+++ lib/exploration/dependency_relation.rb
@@ -15,19 +15,5 @@ module Exploration
         end
       end
     end
-
-    # Yields dependency relationships in +sexp+
-    def get_method_explorers context, &block
-      already_explored = []
-      {
-        [:call] => lambda do |sexp| # search for call statements
-          call_body = sexp.rest
-
-          # by exploring :colon2 first, we won't pick up any :const that was inside a :colon2
-          explore_entity_sexp :colon2, sexp, :dependency, context, already_explored, &block
-          explore_entity_sexp :const, sexp, :dependency, context, already_explored, &block
-        end
-      }
-    end
   end
 end
